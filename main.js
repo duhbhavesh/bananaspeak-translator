967436e23f5db4c1ab9438cfe6fe7979af9d46f6
Bhavesh Kasturi <connect.bhavesh.kasturi@gmail.com>
2020-12-05 22:29:10 +0530

    Update translator

---
+++ main.js
@@ -0,0 +1,11 @@
+let inputText = document.querySelector("#input-text");
+let outputText = document.querySelector("#output-text");
+let btnTranslator = document.querySelector("#btn-translate")
+
+let server = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+
+btnTranslator.addEventListener('click', function hello() {
+  alert("You clickd!");
+})
+
